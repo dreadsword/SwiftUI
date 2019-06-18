682f78d1be907d153dbcdc1cf2104e282d775d59
Robert Dodson <rad@robdodson.net>
2019-06-18 12:47:16 -0700

    checkin

---
+++ FrogRadioSwiftUI/Link.swift
@@ -0,0 +1,29 @@
+//
+//  Link.swift
+//  FrogRadioSwiftUI
+//
+//  Created by Robert Dodson on 6/18/19.
+//  Copyright © 2019 Robert Dodson. All rights reserved.
+//
+
+import SwiftUI
+
+struct Link : View {
+    
+    var linkName: String
+    var linkURL: URL
+    
+    var body: some View {
+        Text(linkName)
+        .color(.blue)
+        .underline()
+    }
+}
+
+#if DEBUG
+struct Link_Previews : PreviewProvider {
+    static var previews: some View {
+        Link(linkName: "link", linkURL: URL(fileURLWithPath: "https://frogradio.net"))
+    }
+}
+#endif
--- FrogRadioSwiftUI/SettingsView.swift
+++ FrogRadioSwiftUI/SettingsView.swift
@@ -19,21 +19,22 @@ struct SettingsView : View {
         VStack {
             Text("Settings")
                .bold()
-               .color(.green)
+               .color(settings.tintColor)
             List {
-                Section(header: Text("General Settings")) {
+                Section(header: Text("General Settings").bold()) {
                     Switch(title:"Show Album Art", selected:settings.showArt)
                     Switch(title:"Send Anonymous Usage Data", selected:settings.sendAnonData)
                     Switch(title:"Levels Meter",selected:settings.levelsMeter)
                     DoubleSwitch(title1:"Dark Mode", selected1: settings.darkMode, title2:"Follow System", selected2: settings.followSystemMode)
                     TintPalette(settings:settings)
                 }
-                Section(header: Text("Support")) {
-                    Text("Example Row")
-                    Text("Example Row")
-                    Text("Example Row")
+                Section(header: Text("Support").bold()) {
+                    Link(linkName: "FrogRadio Website", linkURL: URL(fileURLWithPath: "aa"))
+                    Link(linkName: "Support", linkURL: URL(fileURLWithPath: "bb"))
+                    Link(linkName: "Privacy Policy", linkURL: URL(fileURLWithPath: "cc"))
+                    Link(linkName: "Email Support Request", linkURL: URL(fileURLWithPath: "dd"))
                 }
-                Section(header: Text("Actions")) {
+                Section(header: Text("Actions").bold()) {
                     Text("Example Row")
                     Text("Example Row")
                     Text("Example Row")
